1e3f01f178aac4f337018db08ba9a4cb1af29a99
kfwong <kfwong@zoho.com>
2017-02-11 22:03:14 +0800

    Added part 2. Adjusted makefile to build separate projects.

---
+++ prog.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <zconf.h>
+#include <wait.h>
+#include <stdlib.h>
+
+int main() {
+    pid_t pid = fork();
+    pid_t status = NULL;
+
+    if(pid == 0){
+        char *cmd = "/usr/bin/env";
+        char *argv[2];
+
+        argv[0] = "/usr/bin/env";
+        argv[1] = NULL;
+
+        execvp(cmd, argv);
+    }else{
+
+        waitpid(-1, &status, 0);
+
+        printf("\n");
+
+        char *shell_path = getenv("SHELL_PATH");
+
+        if(shell_path){
+            printf("SHELL_PATH value is %s", shell_path);
+        }else{
+            printf("SHELL_PATH value is UNKNOWN");
+        }
+    }
+
+    return 0;
+}
